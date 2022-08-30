b8718a6989c2e4ee0dd4d821a4b3afd1af9c49ae
yuuu <okazaki@fusic.co.jp>
2022-08-31 05:37:19 +0900

    add hosting

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  auth: {
+    enviiewerf9add888: {
+      IdentityPoolId: "string";
+      IdentityPoolName: "string";
+      UserPoolId: "string";
+      UserPoolArn: "string";
+      UserPoolName: "string";
+      AppClientIDWeb: "string";
+      AppClientID: "string";
+    };
+  };
+};
