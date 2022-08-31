c707e0c27b9b22cb1bf16446232896057e571f2b
yuuu <okazaki@fusic.co.jp>
2022-09-01 06:48:49 +0900

    add hooks

--- src/hooks/useDevices.ts
+++ src/hooks/useDevices.ts
@@ -1,40 +1,112 @@
 import { toast } from "react-toastify";
-import devices from "../data/devices";
 import Device from "../types/device";
-import CreateDeviceInput from "../types/createDeviceInput";
-import UpdateDeviceInput from "../types/updateDeviceInput";
+import { API } from "aws-amplify";
+import { GraphQLResult } from "@aws-amplify/api";
+import { useQuery } from "@tanstack/react-query";
+import {
+  ListDevicesQuery,
+  GetDeviceQuery,
+  CreateDeviceMutation,
+  CreateDeviceInput,
+  DeleteDeviceMutation,
+} from "../../src/API";
+import * as queries from "../../src/graphql/queries";
+import * as mutations from "../../src/graphql/mutations";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export const useFetchDevices = (): Device[] => {
+  const { data: devices } = useQuery(["devices"], async () => {
+    const { data } = (await API.graphql({
+      query: queries.listDevices,
+    })) as GraphQLResult<ListDevicesQuery>;
+    return data?.listDevices?.items.flatMap((d) => (d === null ? [] : [d]));
+  });
   return devices as Device[];
 };
 
 export const useFetchDevice = (id: string): Device => {
-  const device = devices.find((device) => device.id === id);
+  const { data: device } = useQuery(["device"], async () => {
+    const { data } = (await API.graphql({
+      query: queries.getDevice,
+      variables: { id },
+    })) as GraphQLResult<GetDeviceQuery>;
+    return data?.getDevice;
+  });
   return device as Device;
 };
 
 export const useCreateDevice = (onSuccess: () => void) => {
-  return (input: CreateDeviceInput) => {
-    console.log(input);
-    toast.success(`${input.name}を作成しました`);
-    onSuccess();
-  };
+  const queryClient = useQueryClient();
+  const mutation = useMutation(
+    async (input: CreateDeviceInput) => {
+      const { data } = (await API.graphql({
+        query: mutations.createDevice,
+        variables: { input },
+      })) as GraphQLResult<CreateDeviceMutation>;
+      return data;
+    },
+    {
+      onSuccess: (data) => {
+        queryClient.invalidateQueries(["devices"]);
+        toast.success(`${data?.createDevice?.name}を作成しました`);
+        onSuccess();
+      },
+      onError: () => {
+        toast.error("デバイスの作成に失敗しました");
+      },
+    }
+  );
+  return mutation.mutate;
 };
 
 export const useUpdateDevice = (onSuccess: () => void) => {
-  return (input: UpdateDeviceInput) => {
-    console.log(input);
-    toast.success(`${input.name}を更新しました`);
-    onSuccess();
-  };
+  const queryClient = useQueryClient();
+  const mutation = useMutation(
+    async (input: UpdateDeviceInput) => {
+      const { data } = (await API.graphql({
+        query: mutations.updateDevice,
+        variables: { input },
+      })) as GraphQLResult<UpdateDeviceMutation>;
+      return data;
+    },
+    {
+      onSuccess: (data) => {
+        toast.success(`${data?.updateDevice?.name}を更新しました`);
+        queryClient.invalidateQueries(["devices"]);
+        queryClient.invalidateQueries(["device"]);
+        onSuccess();
+      },
+      onError: () => {
+        toast.error("デバイスの更新に失敗しました");
+      },
+    }
+  );
+  return mutation.mutate;
 };
 
 export const useDeleteDevice = (
   device: Device | null,
   onSuccess: () => void
 ) => {
-  return () => {
-    toast.success(`${device?.name}を削除しました`);
-    onSuccess();
-  };
+  const queryClient = useQueryClient();
+  const mutation = useMutation(
+    async () => {
+      const { data } = (await API.graphql({
+        query: mutations.deleteDevice,
+        variables: { input: { id: device?.id } },
+      })) as GraphQLResult<DeleteDeviceMutation>;
+      return data;
+    },
+    {
+      onSuccess: (data) => {
+        toast.success(`${data?.deleteDevice?.name}を削除しました`);
+        queryClient.invalidateQueries(["devices"]);
+        onSuccess();
+      },
+      onError: () => {
+        toast.error("デバイスの削除に失敗しました");
+      },
+    }
+  );
+  return mutation.mutate;
 };
